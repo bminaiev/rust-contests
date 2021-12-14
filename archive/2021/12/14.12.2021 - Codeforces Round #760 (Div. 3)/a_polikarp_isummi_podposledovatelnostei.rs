a3b3284bbde91d3b3dc77b36032f1a062327c8c9
Borys <mb.787788@gmail.com>
2021-12-14 18:36:48 +0000

    add new task

---
+++ archive/2021/12/14.12.2021 - Codeforces Round #760 (Div. 3)/a_polikarp_isummi_podposledovatelnostei.rs
@@ -0,0 +1,30 @@
+//{"name":"A. Поликарп и суммы подпоследовательностей","group":"Codeforces - Codeforces Round #760 (Div. 3)","url":"https://codeforces.com/contest/1618/problem/A","interactive":false,"timeLimit":1000,"tests":[{"input":"5\n1 3 4 4 5 7 8\n1 2 3 4 5 6 7\n300000000 300000000 300000000 600000000 600000000 600000000 900000000\n1 1 2 999999998 999999999 999999999 1000000000\n1 2 2 3 3 4 5\n","output":"1 4 3\n4 1 2\n300000000 300000000 300000000\n999999998 1 1\n1 2 2\n"}],"testType":"multiNumber","input":{"type":"stdin","fileName":null,"pattern":null},"output":{"type":"stdout","fileName":null,"pattern":null},"languages":{"java":{"taskClass":"APolikarpISummiPodposledovatelnostei"}}}
+
+use algo_lib::io::input::Input;
+use algo_lib::io::output::output;
+use algo_lib::{out, out_line};
+
+fn solve(input: &mut Input, _test_case: usize) {
+    let n = 7;
+    let a = input.read_vec::<i64>(n);
+    let a3 = a.last().unwrap() - a[0] - a[1];
+    out_line!(format!("{} {} {}", a[0], a[1], a3));
+}
+
+pub(crate) fn run(mut input: Input) -> bool {
+    let t = input.read();
+    for i in 0usize..t {
+        solve(&mut input, i + 1);
+    }
+    output().flush();
+    input.skip_whitespace();
+    !input.peek().is_some()
+}
+
+//START MAIN
+mod tester;
+
+fn main() {
+    tester::run_tests();
+}
+//END MAIN
