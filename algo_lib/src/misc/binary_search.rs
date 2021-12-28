6355d6733c669dfe7b348724ffc7bedbdaba173c
Borys <mb.787788@gmail.com>
2021-12-28 23:36:04 +0000

    Borys, doing competitive programming for 10 years, still don't know how to write binary search correctly.

--- algo_lib/src/misc/binary_search.rs
+++ algo_lib/src/misc/binary_search.rs
@@ -5,14 +5,15 @@ pub fn binary_search_first_true<T>(range: Range<T>, mut f: impl FnMut(T) -> bool
 where
     T: Number,
 {
-    let mut left = range.start;
+    // we can't store [range.start - 1] into [left], because it could overflow
+    let mut left_plus_one = range.start;
     let mut right = range.end;
-    while right - left > T::ONE {
-        let mid = left + (right - left) / T::TWO;
+    while right > left_plus_one {
+        let mid = left_plus_one + (right - left_plus_one) / T::TWO;
         if f(mid) {
             right = mid;
         } else {
-            left = mid;
+            left_plus_one = mid + T::ONE;
         }
     }
     right
@@ -27,5 +28,24 @@ where
         None
     } else {
         Some(first_false - T::ONE)
+    }
+}
+
+#[test]
+fn simple_stress() {
+    const N: usize = 50;
+    for n in 1..N {
+        for cnt_false in 0..=n {
+            let mut a = vec![false; cnt_false];
+            a.resize(n, true);
+            let mut max_f_calls = ((n + 1) as f64).log2().ceil() as i32;
+            let f_is_true = |id: usize| -> bool {
+                max_f_calls -= 1;
+                assert!(max_f_calls >= 0);
+                a[id]
+            };
+            let result = binary_search_first_true(0..n, f_is_true);
+            assert_eq!(result, cnt_false);
+        }
     }
 }
