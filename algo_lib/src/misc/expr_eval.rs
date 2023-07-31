c2f43e132fec39f110d47dad32b5d0b9dec2f63b
Borys Minaiev <mb.787788@gmail.com>
2023-07-31 20:49:17 +0100

    expression evaluator

---
+++ algo_lib/src/misc/expr_eval.rs
@@ -0,0 +1,66 @@
+fn process_op(st: &mut Vec<i64>, ops: &mut Vec<u8>) {
+    let op = ops.pop().unwrap();
+    let r = st.pop().unwrap();
+    let l = st.pop().unwrap();
+    let res = match op {
+        b'+' => l + r,
+        b'-' => l - r,
+        b'*' => l * r,
+        b'/' => l / r,
+        _ => panic!("Unknown op {}", op),
+    };
+    st.push(res);
+}
+
+fn is_op(c: u8) -> bool {
+    match c {
+        b'+' | b'-' | b'*' | b'/' => true,
+        _ => false,
+    }
+}
+
+fn priority(c: u8) -> i32 {
+    match c {
+        b'+' | b'-' => 1,
+        b'*' | b'/' => 2,
+        b'(' => -1,
+        _ => panic!("Unknown op {}", c as char),
+    }
+}
+
+pub fn eval_expression(s: &[u8]) -> i64 {
+    let mut st = vec![];
+    let mut ops = vec![];
+    let mut iter = 0;
+    while iter < s.len() {
+        let c = s[iter];
+        if c == b'(' {
+            ops.push(c);
+        } else if c == b')' {
+            while *ops.last().unwrap() != b'(' {
+                process_op(&mut st, &mut ops)
+            }
+            ops.pop();
+        } else if is_op(c) {
+            while !ops.is_empty() && priority(*ops.last().unwrap()) >= priority(c) {
+                process_op(&mut st, &mut ops);
+            }
+            ops.push(c);
+        } else {
+            let mut num = 0;
+            while iter < s.len() && s[iter].is_ascii_alphanumeric() {
+                num = num * 10 + (s[iter] - b'0') as i64;
+                iter += 1;
+            }
+            iter -= 1;
+            st.push(num);
+        }
+        iter += 1;
+    }
+
+    while !ops.is_empty() {
+        process_op(&mut st, &mut ops);
+    }
+
+    st[0]
+}
--- archive/2023/7/31.7.2023 - Manual/b.rs
+++ archive/2023/7/31.7.2023 - Manual/b.rs
@@ -4,121 +4,47 @@ use algo_lib::io::output::output;
 use algo_lib::io::task_io_settings::TaskIoType;
 use algo_lib::io::task_runner::run_task;
 use algo_lib::io::{input::Input, task_io_settings::TaskIoSettings};
+use algo_lib::misc::expr_eval::eval_expression;
 #[allow(unused)]
 use algo_lib::{dbg, out, out_line};
 
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
-enum CharOfNumber {
-    Number(i64),
-    Char(u8),
-}
-
-fn eval(s: &[CharOfNumber]) -> i64 {
-    // assert!(!s.contains(&b'('));
-    // assert!(!s.contains(&b')'));
-    let mut ops = vec![];
-    let mut numbers = vec![];
-    let mut cur_number = 0;
-    for i in 0..s.len() {
-        match s[i] {
-            CharOfNumber::Number(x) => {
-                assert_eq!(cur_number, 0);
-                cur_number = x;
-            }
-            CharOfNumber::Char(c) => {
-                if c == b'+' || c == b'*' || c == b'-' {
-                    ops.push(c);
-                    numbers.push(cur_number);
-                    cur_number = 0;
-                } else {
-                    cur_number = cur_number * 10 + (c - b'0') as i64;
-                }
-            }
-        }
-    }
-    numbers.push(cur_number);
-    let mut new_numbers = vec![numbers[0]];
-    let mut new_ops = vec![];
-    for i in 0..ops.len() {
-        if ops[i] == b'*' {
-            let last = new_numbers.pop().unwrap();
-            new_numbers.push(last * numbers[i + 1]);
-        } else {
-            new_ops.push(ops[i]);
-            new_numbers.push(numbers[i + 1]);
-        }
-    }
-    let mut res = new_numbers[0];
-    for i in 0..new_ops.len() {
-        if new_ops[i] == b'+' {
-            res += new_numbers[i + 1];
-        } else {
-            res -= new_numbers[i + 1];
-        }
-    }
-    res
-}
-
-fn eval_rec(s: &[CharOfNumber]) -> i64 {
-    for i in 0..s.len() {
-        if s[i] == CharOfNumber::Char(b'(') {
-            let mut cnt = 1;
-            for j in i + 1.. {
-                if s[j] == CharOfNumber::Char(b'(') {
-                    cnt += 1;
-                }
-                if s[j] == CharOfNumber::Char(b')') {
-                    cnt -= 1;
-                }
-                if cnt == 0 {
-                    let res = eval_rec(&s[i + 1..j]);
-                    let mut new_s = vec![];
-                    for k in 0..i {
-                        new_s.push(s[k]);
-                    }
-                    new_s.push(CharOfNumber::Number(res));
-                    for k in j + 1..s.len() {
-                        new_s.push(s[k]);
-                    }
-                    return eval_rec(&new_s);
-                }
-            }
-        }
-    }
-    eval(s)
-}
-
-fn solve(input: &mut Input, _test_case: usize) {
-    let s = input.string();
-    let expected = input.i64();
+fn find_all_sols(s: &[u8], expected_res: i64) -> Vec<Vec<u8>> {
     let mut res = vec![];
     for a in 0..10 {
         for b in 0..10 {
             for c in 0..10 {
                 for d in 0..10 {
-                    let mut to_check = vec![];
-                    let cur_way = vec![a, b, c, d];
-                    for &x in s.iter() {
-                        if x >= b'a' && x <= b'd' {
-                            let number = cur_way[x as usize - b'a' as usize] + b'0';
-                            to_check.push(CharOfNumber::Char(number));
-                        } else {
-                            to_check.push(CharOfNumber::Char(x));
-                        }
-                    }
-                    if eval_rec(&to_check) == expected {
-                        res.push(cur_way);
+                    let check = vec![a, b, c, d];
+                    let test: Vec<_> = s
+                        .iter()
+                        .map(|&c| {
+                            if c >= b'a' && c <= b'd' {
+                                check[(c - b'a') as usize] + b'0'
+                            } else {
+                                c
+                            }
+                        })
+                        .collect();
+                    if eval_expression(&test) == expected_res {
+                        res.push(check);
                     }
                 }
             }
         }
     }
-    out_line!(res.len());
-    if res.len() == 1 {
-        out_line!(res[0]
-            .iter()
-            .map(|&x| (x + b'0') as char)
-            .collect::<String>());
+    res
+}
+
+fn solve(input: &mut Input, _test_case: usize) {
+    let s = input.string();
+    let expected_res = input.i64();
+    let sols = find_all_sols(&s, expected_res);
+    out_line!(sols.len());
+    if sols.len() == 1 {
+        for x in sols[0].iter() {
+            out!(*x);
+        }
+        out_line!();
     }
 }
 
