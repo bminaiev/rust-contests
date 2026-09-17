13b553f8594ef09c98bfa7a87bdc790a79c9065d
Borys Minaiev <bminaiev@gmail.com>
2026-09-17 12:40:12 +0100

    cf

---
+++ archive/2026/09/2026.09.17 - Educational Codeforces Round 194 (Rated for Div. 2)/a_.rs
@@ -0,0 +1,47 @@
+#[allow(unused)]
+use algo_lib::dbg;
+use algo_lib::io::input::Input;
+use algo_lib::io::output::Output;
+
+fn solve(input: &mut Input, out: &mut Output) {
+    let tc = input.usize();
+    for _ in 0..tc {
+        let n = input.usize();
+        let a = input.vec::<usize>(n);
+        let sum = a.iter().sum::<usize>();
+        if sum + 2 > n {
+            out.println(-1);
+            continue;
+        }
+        let res = a[0] + a[n - 1];
+        out.println(res);
+    }
+}
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    solve(&mut input, &mut output);
+    output.flush();
+    true
+}
+
+#[cfg(feature = "local")]
+fn main() {
+    const PROBLEM_NAME: &str = "a_";
+    #[allow(unused)]
+    use algo_lib::misc::dragon::run_dragon;
+    use algo_lib::tester::helper::*;
+
+    run_tests(PROBLEM_NAME, run);
+    // run_single_test(PROBLEM_NAME, run, "1");
+    // run_stress(stress);
+    // run_locally(run);
+    // run_dragon(solve, "W", 1..2);
+}
+//END MAIN
+
+#[cfg(not(feature = "local"))]
+fn main() {
+    let input = algo_lib::io::input::Input::new_stdin();
+    let mut output = algo_lib::io::output::Output::new_stdout();
+    run(input, output);
+}
---
+++ archive/2026/09/2026.09.17 - Educational Codeforces Round 194 (Rated for Div. 2)/b_.rs
@@ -0,0 +1,53 @@
+#[allow(unused)]
+use algo_lib::dbg;
+use algo_lib::io::input::Input;
+use algo_lib::io::output::Output;
+
+fn solve(input: &mut Input, out: &mut Output) {
+    let tc = input.usize();
+    for _ in 0..tc {
+        let mut x = input.i64();
+        let mut y = input.i64();
+        let k = input.i64();
+        let mut res = 0;
+        for i in 0..k {
+            let now = y % x;
+            res += now;
+            if y / x == 1 {
+                res += (k - i - 1) * now;
+                break;
+            }
+            x += 1;
+            y += 1;
+        }
+        out.println(res);
+    }
+}
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    solve(&mut input, &mut output);
+    output.flush();
+    true
+}
+
+#[cfg(feature = "local")]
+fn main() {
+    const PROBLEM_NAME: &str = "b_";
+    #[allow(unused)]
+    use algo_lib::misc::dragon::run_dragon;
+    use algo_lib::tester::helper::*;
+
+    run_tests(PROBLEM_NAME, run);
+    // run_single_test(PROBLEM_NAME, run, "1");
+    // run_stress(stress);
+    // run_locally(run);
+    // run_dragon(solve, "W", 1..2);
+}
+//END MAIN
+
+#[cfg(not(feature = "local"))]
+fn main() {
+    let input = algo_lib::io::input::Input::new_stdin();
+    let mut output = algo_lib::io::output::Output::new_stdout();
+    run(input, output);
+}
