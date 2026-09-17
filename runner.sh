ad7ab2f73d1acbd3f385cac823c6ba20ef13fa8e
Borys Minaiev <bminaiev@gmail.com>
2026-09-17 21:11:04 +0100

    update runner

--- runner.sh
+++ runner.sh
@@ -2,4 +2,12 @@
 
 project=$(echo "$1" | tr '/' ' ' | awk '{print $2 }')
 echo "Running $project"
-RUST_BACKTRACE=1 cargo run --bin "$project" "${@:2}"
+
+cargo build --bin "$project" || exit $?
+
+(
+    # These limits are specified in KiB.
+    ulimit -v $((2 * 1024 * 1024))
+    ulimit -s $((512 * 1024))
+    RUST_BACKTRACE=1 exec "./target/debug/$project" "${@:2}"
+)
